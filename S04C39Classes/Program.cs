848b99ee77974caa91bb4f3b8ace25994ca2366c
Tiago Sathler <sathler@gmail.com>
2023-10-03 16:02:46 -0300

    :sparkles: feat: finaliza o exercício 01 da S04C39 - Classes, atributos e métodos

---
+++ S04C39Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace S04S39Classes;
+
+using S04C39Classes;
+using System.Globalization;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        Exercise01();
+    }
+
+    private static void Exercise01()
+    {
+        Rectangle rectangle = new Rectangle();
+        Console.WriteLine("Entre com a largura e altura do retângulo:");
+        rectangle.Width = double.Parse(Console.ReadLine()!);
+        rectangle.Height = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("ÁREA = {0:F2}", rectangle.Area());
+        Console.WriteLine("PERÍMETRO = {0:F2}", rectangle.Perimeter());
+        Console.WriteLine("DIAGONAL = {0:F2}", rectangle.Diagonal());
+    }
+}
---
+++ S04C39Classes/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿namespace S04C39Classes;
+
+internal class Rectangle
+{
+    public double Width;
+    public double Height;
+
+    public double Area()
+    {
+        return Width * Height;
+    }
+
+    public double Perimeter()
+    {
+        return (Width + Height) * 2;
+    }
+
+    public double Diagonal()
+    {
+        return Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+    }
+}
