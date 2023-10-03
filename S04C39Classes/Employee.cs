cdbc1bb59a790e9a425a82faf3c8b2c83d9f07a3
Tiago Sathler <sathler@gmail.com>
2023-10-03 17:05:31 -0300

    :sparkles: feat: finaliza o exercício 2 da S04C39 - Classes, atributos e métodos

---
+++ S04C39Classes/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace S04C39Classes;
+
+internal class Employee
+{
+    public string ?Name { get; set; }
+    public double GrossSalary { get; set; }
+    public double Tax { get; set; }
+
+    public double NetSalary()
+    {
+        return GrossSalary - Tax;
+    }
+
+    public void IncreaseSalary(double percentage)
+    {
+        GrossSalary *= (1 + percentage / 100);
+    }
+
+    override public string ToString()
+    {
+        return $"{Name}, $ {NetSalary():F2}";
+    }
+}
--- S04C39Classes/Program.cs
+++ S04C39Classes/Program.cs
@@ -8,7 +8,8 @@ internal static class Program
     private static void Main(string[] args)
     {
         Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-        Exercise01();
+        // Exercise01();
+        Exercise02();
     }
 
     private static void Exercise01()
@@ -21,5 +22,23 @@ internal static class Program
         Console.WriteLine("ÁREA = {0:F2}", rectangle.Area());
         Console.WriteLine("PERÍMETRO = {0:F2}", rectangle.Perimeter());
         Console.WriteLine("DIAGONAL = {0:F2}", rectangle.Diagonal());
+    }
+
+    private static void Exercise02()
+    {
+        Employee employee = new Employee();
+        Console.Write("Nome: ");
+        employee.Name = Console.ReadLine();
+        Console.Write("Salário bruto: ");
+        employee.GrossSalary = double.Parse(Console.ReadLine()!);
+        Console.Write("Imposto: ");
+        employee.Tax = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine($"\nFuncionário: {employee}");
+
+        Console.Write("\nDigite a porcentagem para aumentar o salário: ");
+        employee.IncreaseSalary(double.Parse(Console.ReadLine()!));
+
+        Console.WriteLine($"\nFuncionário: {employee}");
     }
 }
