8edd03c49a2ba7f49308adca014a63951ed54ba8
Tiago Sathler <sathler@gmail.com>
2023-10-04 16:47:24 -0300

    :sparkles: feat: conclui o Exercício de fixação da S05C60

---
+++ S05C60/Account.cs
@@ -0,0 +1,53 @@
+﻿namespace S05C60
+{
+    internal class Account
+    {
+        private const double WITHDRAWAL_FEE = 5.0;
+
+        public string? Holder { get; set; }
+
+        public int Number { get; }
+
+        public double Balance { get; private set; }
+
+        public Account(int number)
+        {
+            Balance = 0;
+            Number = number;
+        }
+
+        public Account(string holder, int number) : this(number)
+        {
+            Holder = holder;
+        }
+
+        public void Deposit(double value)
+        {
+            if (value > 0)
+            {
+                Balance += value;
+            }
+            else
+            {
+                Console.WriteLine("Depósito inválido!");
+            }
+        }
+
+        public void Withdrawal(double amount)
+        {
+            if (Balance >= amount + WITHDRAWAL_FEE)
+            {
+                Balance -= amount + WITHDRAWAL_FEE;
+            }
+            else
+            {
+                Console.WriteLine("Não há saldo suficiente para sacar!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Conta bancária número: {Number}; Titular: {Holder}; Saldo $ {Balance:F2}";
+        }
+    }
+}
---
+++ S05C60/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace S05C60;
+
+using System.Globalization;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        Console.Write("Entre com o número da conta: ");
+        Account account = new(int.Parse(Console.ReadLine()!));
+
+        Console.Write("Entre com o titular da conta: ");
+        account.Holder = Console.ReadLine();
+
+        Console.Write("Haverá depósito inicial (s/n)? ");
+        string choice = Console.ReadLine()!.Trim().ToLower();
+
+        if (choice.Length > 0 && choice[0] == 's')
+        {
+            Console.Write("Entre com o valor do depósito inicial: ");
+            account.Deposit(double.Parse(Console.ReadLine()!));
+        }
+
+        Console.WriteLine($"\nDados da conta\n{account}\n");
+
+        Console.Write("Entre com um valor para depósito: ");
+        account.Deposit(double.Parse(Console.ReadLine()!));
+        Console.WriteLine($"Dados da conta atualizada\n{account}\n");
+
+        Console.Write("Entre com um valor para saque: ");
+        account.Withdrawal(double.Parse(Console.ReadLine()!));
+        Console.WriteLine($"Dados da conta atualizada\n{account}\n");
+    }
+}
