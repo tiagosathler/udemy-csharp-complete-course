2d3733ebd518651f4a77fc3e8304bcb9d3ecf3e7
Tiago Sathler <sathler@gmail.com>
2023-10-13 09:21:05 -0300

    :sparkles: feat: conclui o Exercício da S10C140
    Exercício sobre Herança, Polimorfismo, Upcasting, Sobrescrita, Encapsulamento e Classe selada

---
+++ S10C140/Entities/OutsourceEmployee.cs
@@ -0,0 +1,23 @@
+﻿namespace S10C140.Entities;
+
+internal sealed class OutsourceEmployee : Employee
+{
+    private const double ADDITIONAL_TAX = 1.1;
+
+    public double AdditionalCharge { get; set; }
+
+    public OutsourceEmployee()
+    {
+    }
+
+    public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+        : base(name, hours, valuePerHour)
+    {
+        AdditionalCharge = additionalCharge;
+    }
+
+    public override double Payment()
+    {
+        return base.Payment() + (AdditionalCharge * ADDITIONAL_TAX);
+    }
+}
---
+++ S10C140/Program.cs
@@ -0,0 +1,53 @@
+﻿using S10C140.Entities;
+
+namespace S10C140;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+        List<Employee> employees = new();
+
+        Console.Write("Enter the number of employees: ");
+        int.TryParse(Console.ReadLine(), out int numberOfEmployees);
+
+        for (int i = 1; i <= numberOfEmployees; i++)
+        {
+            Console.WriteLine($"\n\x1b[1mEmployee #{i} data:\x1b[0m");
+
+            Console.Write("Outsourced (y/n)? ");
+            string? choosenOutsourced = Console.ReadLine();
+            choosenOutsourced = String.IsNullOrWhiteSpace(choosenOutsourced) ? "n" : choosenOutsourced.Trim().ToLower();
+
+            Console.Write("Name: ");
+            string? name = Console.ReadLine();
+            name = String.IsNullOrWhiteSpace(name) ? "EMPTY NAME" : name.Trim();
+
+            Console.Write("Hours: ");
+            int.TryParse(Console.ReadLine(), out int hours);
+
+            Console.Write("Value per hour: ");
+            double.TryParse(Console.ReadLine(), out double valuePerHour);
+
+            if (choosenOutsourced.StartsWith('y'))
+            {
+                Console.Write("Additional charge: ");
+                double.TryParse(Console.ReadLine(), out double additionalCharge);
+
+                employees.Add(new OutsourceEmployee(name, hours, valuePerHour, additionalCharge));
+            }
+            else
+            {
+                employees.Add(new Employee(name, hours, valuePerHour));
+            }
+        }
+
+        Console.WriteLine("\nPAYMENTS:");
+        foreach (Employee e in employees)
+        {
+            Console.WriteLine($"{e.Name} - $ {e.Payment():F2}");
+        }
+    }
+}
